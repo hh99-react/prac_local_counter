578ecea100bb4a79969a3dc462766d91096155d4
with-key <rem9238@kakao.com>
2022-07-14 01:52:32 +0900

    counter 만들기 실습 완료

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{number}</div>
+      <button
+        onClick={() => {
+          setNumber(number + 1);
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          setNumber(number - 1);
+        }}
+      >
+        -
+      </button>
     </div>
   );
 }
